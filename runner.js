86c28de2eeb59eaf97694515f62942d45e4656bd
satilpereira <satil.prog@gmail.com>
2024-02-14 17:11:31 -0300

    feat(Creation): improved app creation
    add .gitignore file to created aplication
    Update package dependencies and improve application creation process

--- runner.js
+++ runner.js
@@ -6,6 +6,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import ncp from "ncp";
 import { exec } from "child_process";
+import fs from "fs";
+import { Spinner } from "cli-spinner";
+import chalk from "chalk";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,7 +16,7 @@ const ncpAsync = promisify(ncp);
 const execAsync = promisify(exec);
 
 program
-  .version("1.0.4")
+  .version("1.0.5")
   .command("create [projectName]")
   .description(
     "Create a new application in the specified directory or the current directory if not provided"
@@ -22,11 +25,11 @@ program
     const sourcePath = path.join(__dirname, "src");
     const destinationPath = path.join(process.cwd(), projectName);
 
-    console.log(
-      `Creating ${
+    console.log(chalk.blue(
+      `🛈 Creating ${
         projectName === "." ? "the current directory" : projectName
       }...`
-    );
+    ));
 
     // Use ncp to copy the content of the src directory
     try {
@@ -35,18 +38,54 @@ program
         stopOnErr: true, // Stop on the first error encountered
       });
 
-      console.log("Application files copied successfully!");
+      console.log(
+        chalk.green(
+        "✓ Application files copied successfully!"));
+
+      // Check if .gitignore exists in the src directory
+      const gitignorePath = path.join(sourcePath, ".gitignore");
+      if (fs.existsSync(gitignorePath)) {
+        // Copy .gitignore to the destination directory
+        const destGitignorePath = path.join(destinationPath, ".gitignore");
+        await ncpAsync(gitignorePath, destGitignorePath);
+      }
+
 
       // Change the working directory to the destination path
       process.chdir(destinationPath);
 
+      const spinner = new Spinner({
+        text: (
+          "\x1b[34m" +
+          "Installing dependencies, please wait..."),
+        onTick: function (msg) {
+          this.clearLine(this.stream);
+          this.stream.write(msg);
+        },
+      });
+      spinner.setSpinnerString(18);
+
+      // Logs("Installing dependencies, please wait...");
+      spinner.start();
+      
       // Run npm install
-      console.log("Running npm install...");
       await execAsync("npm install");
 
-      console.log("npm install completed successfully!");
+      // Stop the spinner
+      spinner.stop(true);
+
+      console.log(chalk.green("✓ Application created successfully!"));
+      console.group();
+      console.log(chalk.blue.bold("To get started, try running:"));
+      
+      // If different folder, add folder name to command
+      if (projectName !== ".") {
+        console.log(chalk.blue.bold(`cd ${projectName}`));
+      }
+      console.log(chalk.blue.bold("npm run dev"));
+      console.groupEnd();
     } catch (error) {
-      console.error("Error creating application:", error.message);
+      console.error(chalk.red("⚠️ Error creating application:", error.message));
     }
   });
 
