0212aa6b8461e25d921cff6acdadd51c58159d92
satilpereira <satil.prog@gmail.com>
2024-04-28 21:16:52 -0300

    refactor(favicon): Updade page.tsx and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import cn from '@utils/cn'
 
 const page = () => {
   return (
-    <div className='mx-auto flex max-w-7xl flex-col justify-between gap-2 p-4  md:h-screen md:max-h-[764px] md:gap-4'>
+    <div className='mx-auto flex w-full max-w-7xl flex-col justify-between gap-2 p-4 md:gap-4'>
       <div>
         Get started by editing <code>app/page.tsx</code>
       </div>
